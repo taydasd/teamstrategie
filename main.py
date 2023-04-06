752c538889e820a3a1e3e0c74ad9dc16c4f9869a
lukask005 <92630615+lukask005@users.noreply.github.com>
2023-04-06 21:24:09 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,1 @@
-from StepperController.StepperController import StepperController
-from Camera.Camera import Camera
-controller = StepperController("COM4", 115200)
-controller.calibrate()
-camera = Camera(0)
 
