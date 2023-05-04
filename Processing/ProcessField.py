c72a45a99d98864d7c3e116f495c6dee26a4590d
Lukas Karg <lukas.karg@googlemail.com>
2023-05-04 12:01:12 +0200

    added process field

--- Processing/ProcessField.py
+++ Processing/ProcessField.py
@@ -2,6 +2,7 @@ class ProcessField:
     def __init__(self, max_x, max_y):
         self.max_x = max_x  # max values from StepperControllerClass
         self.max_y = max_y
+    def set_img_offset(self, x0,y0,y1,y2):
 
     def get_real_pos_from_img_pos(self, x, y):
         return {x, y}  # not implemented yet
