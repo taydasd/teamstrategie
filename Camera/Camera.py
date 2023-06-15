83a7ae07da5db1720a07ca211ecf45bb781e4ef0
Lukas Karg <lukas.karg@googlemail.com>
2023-06-15 13:07:42 +0200

    enabled opencl for hw accel

--- Camera/Camera.py
+++ Camera/Camera.py
@@ -2,15 +2,17 @@ import os
 
 import cv2
 from threading import Thread
+import time
 
 
 class Camera:
     def __init__(
-        self, camera_index, frame_width, frame_height, focus, buffer_size, fps
+            self, camera_index, frame_width, frame_height, focus, buffer_size, fps
     ):
+        self.fps = fps
         self.stream = cv2.VideoCapture(camera_index, cv2.CAP_DSHOW)
         self.stream.set(cv2.CAP_PROP_HW_ACCELERATION, cv2.VIDEO_ACCELERATION_ANY)
-        self.stream.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('X', 'V', 'I', 'D'))
+        self.stream.set(cv2.CAP_PROP_FOURCC, cv2.VideoWriter_fourcc('P', 'M', 'I', '1'))
         self.stream.set(cv2.CAP_PROP_FRAME_WIDTH, frame_width)
         self.stream.set(cv2.CAP_PROP_FRAME_HEIGHT, frame_height)
         self.stream.set(cv2.CAP_PROP_FPS, fps)
@@ -31,12 +33,17 @@ class Camera:
         return self.frame
 
     def get_next_frame(self):
+        fps = self.fps  # Desired frame rate
+        frame_time = 1 / fps
         while not self.stopped:
+            start_time = time.time()
             if not self.grabbed:
                 self.stop()
             else:
                 (self.grabbed, self.frame) = self.stream.read()
                 self.new_frame = True
+            elapsed_time = time.time() - start_time
+            time.sleep(max(0, frame_time - elapsed_time))
 
     def stop(self):
         self.stopped = True
