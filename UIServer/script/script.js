276697c7c1a99ea9d3e3baf2741dd985fb695a72
matusoo <matus.skvar@gmail.com>
2024-05-21 18:42:31 +0200

    Neue Spielstandanzeige implementiert

--- UIServer/script/script.js
+++ UIServer/script/script.js
@@ -2,72 +2,76 @@ var gameStopped = true;
 const gameTime = 600; // 10 Mins
 
 setInterval(() => {
-    fetch("state").then((res) => res.json().then((json) => {
-        gpio5.value += json.gpio5; //bot?
-        gpio6.value += json.gpio6; //prof?
-        
-        var difference = Math.abs(gpio5.value - gpio6.value);
+  fetch("state").then((res) =>
+    res.json().then((json) => {
+      gpio5.value += json.gpio5; //bot?
+      gpio6.value += json.gpio6; //prof?
 
-        if (gpio5.value > gpio6.value && difference > 2) {
-            godlikeAudio.play();
-            playGIF('bot');
-        } else if (gpio5.value < gpio6.value && difference > 2) {
-            unstoppableAudio.play();
-            playGIF('prof');
-        }
-    }))
+      var difference = Math.abs(gpio5.value - gpio6.value);
+
+      if (gpio5.value > gpio6.value && difference > 2) {
+        godlikeAudio.play();
+        playGIF("bot");
+      } else if (gpio5.value < gpio6.value && difference > 2) {
+        unstoppableAudio.play();
+        playGIF("prof");
+      }
+    })
+  );
 }, 500);
 
 function startGame() {
-    startAudio.play();
-    backgroundAudio.play();
-    gameStopped = false;
-    startTimer(gameTime);
-};
+  document.getElementById("name-input").readOnly = true;
+  startAudio.play();
+  backgroundAudio.play();
+  gameStopped = false;
+  startTimer(gameTime);
+}
 
 function stopGame() {
-    backgroundAudio.pause();
-    backgroundAudio.currentTime = 0;
-    gameStopped = true;
-};
+  document.getElementById("name-input").readOnly = false;
+  backgroundAudio.pause();
+  backgroundAudio.currentTime = 0;
+  gameStopped = true;
+}
 
 function startTimer(duration) {
-    var timer = duration;
-    var minutes, seconds;
-    var intervalId;
+  var timer = duration;
+  var minutes, seconds;
+  var intervalId;
 
-    intervalId = setInterval(function () {
-        minutes = parseInt(timer / 60, 10);
-        seconds = parseInt(timer % 60, 10);
+  intervalId = setInterval(function () {
+    minutes = parseInt(timer / 60, 10);
+    seconds = parseInt(timer % 60, 10);
 
-        minutes = minutes < 10 ? "0" + minutes : minutes;
-        seconds = seconds < 10 ? "0" + seconds : seconds;
+    minutes = minutes < 10 ? "0" + minutes : minutes;
+    seconds = seconds < 10 ? "0" + seconds : seconds;
 
-        countdown.value = minutes + ":" + seconds;
+    countdown.value = minutes + ":" + seconds;
 
-        if (--timer < 0) {
-            timer = duration;
-        }
-
-        if (gameStopped) {
-            clearInterval(intervalId);
-        }
-    }, 1000);
-};
-
-function playGIF(player) {
-    const gifElement = document.getElementById('score-gif');
-    const scoreboard = document.getElementById('scoreboard');
-
-    if (player === 'prof') {
-        gifElement.src = 'resources/gifs/pulp.gif';
-    } else if (player === 'bot') {
-        gifElement.src = 'resources/gifs/losingteeth.gif';
+    if (--timer < 0) {
+      timer = duration;
     }
 
-    scoreboard.style.display = 'block';
+    if (gameStopped) {
+      clearInterval(intervalId);
+    }
+  }, 1000);
+}
 
-    setTimeout(() => {
-        scoreboard.style.display = 'none';
-    }, 3000);
-};
+function playGIF(player) {
+  const gifElement = document.getElementById("score-gif");
+  const scoreboard = document.getElementById("scoreboard");
+
+  if (player === "prof") {
+    gifElement.src = "resources/gifs/pulp.gif";
+  } else if (player === "bot") {
+    gifElement.src = "resources/gifs/losingteeth.gif";
+  }
+
+  scoreboard.style.display = "block";
+
+  setTimeout(() => {
+    scoreboard.style.display = "none";
+  }, 3000);
+}
--- UIServer/server.js
+++ UIServer/server.js
@@ -1,24 +1,24 @@
-const express = require('express');
-const rpio = require('rpio');
+const express = require("express");
+// const rpio = require('rpio');
 
 const app = express();
 
 const PORT = 4321;
 
-rpio.init({mapping: 'gpio'});
-rpio.open(5, rpio.INPUT);
-rpio.open(6, rpio.INPUT);
+// rpio.init({mapping: 'gpio'});
+// rpio.open(5, rpio.INPUT);
+// rpio.open(6, rpio.INPUT);
 
-app.use('/resources', express.static('resources'));
-app.use('/script', express.static('script'));
-app.use('/style', express.static('style'));
+app.use("/resources", express.static("resources"));
+app.use("/script", express.static("script"));
+app.use("/style", express.static("style"));
 
-app.get('/', (req, res) => {
-    res.sendFile('index.html', { root: __dirname });
+app.get("/", (req, res) => {
+  res.sendFile("index.html", { root: __dirname });
 });
 
-app.get('/state', (req, res) => {
-    res.json({"gpio5": rpio.read(5), "gpio6": rpio.read(6)});
-});
+// app.get('/state', (req, res) => {
+//     res.json({"gpio5": rpio.read(5), "gpio6": rpio.read(6)});
+// });
 
 app.listen(PORT, () => console.log("Server listening on port", PORT));
